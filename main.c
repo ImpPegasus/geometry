f9dd2e0804e74e50511005fbc87d462c108548fd
Pegasus <itsokaymate@yandex.ru>
2019-05-18 12:35:46 +0700

    main modified

--- main.c
+++ main.c
@@ -10,13 +10,18 @@ int Plo (unsigned int r){
 return PI*r*r;
 }
 
+
 int main(){
-float o1,o2;
+float o1=0,o2;
 unsigned int o3;
+//char m[];
 
 printf ("Задайте необходимую фигуру \n");
 scanf ("%f %f %d", &o1,&o2,&o3);
+//scanf("%20s", &m);
 
 printf("Фигура: %f %f %d \n Периметр: %d \n Площадь: %d \n", o1, o2, o3, Per(o3), Plo(o3));
 
+return 0;
 }
+
