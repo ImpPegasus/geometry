4012b9bbf81899afadafc8789f8ebaf54604878b
Pegasus <itsokaymate@yandex.ru>
2019-05-18 07:06:44 +0700

    geometry fix

---
+++ geometry.h
@@ -0,0 +1,21 @@
+#ifdef GEOMETRY_H
+#define GEOMETRY_H
+#include <stdio.h>
+
+typedef struct {
+float x,y;
+
+}point;
+
+typedef struct {
+point o;
+int r
+}circle;
+
+circle test;
+point z;
+
+int Per(int r);
+int Plo(int r);
+
+#endif
